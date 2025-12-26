0d1528fe3cdcb04c658c3024e5e83fc767353e59
OtavioYukio <yukiootavio@gmail.com>
2025-12-26 12:28:58 -0300

    feat: add ProductService object in the class ProductController

---
+++ src/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import java.util.Date;
+
+import entities.Product;
+import services.ProductService;
+
+public class ProductController {
+    private ProductService productService = new ProductService();
+}
