bdfaef0e62dccd73f889a1908f4ee01a8d9c555e
OtavioYukio <yukiootavio@gmail.com>
2025-12-26 13:47:56 -0300

    feat: add the generateId method to the ProductIDGenerator and delete the redundant method defineId

--- src/services/IDGenerator.java
+++ src/services/IDGenerator.java
@@ -5,6 +5,5 @@ import java.util.List;
 public abstract class IDGenerator<T> {
     protected Boolean isUsing = false;
     protected abstract void setUsageStatus(List<T> entities, Integer generatedId);
-    protected abstract Integer generateId();
-    public abstract void defineId(T entity);
+    public abstract Integer generateId(List<T> entities);
 }
--- src/services/ProductIDGenerator.java
+++ src/services/ProductIDGenerator.java
@@ -1,17 +1,30 @@
 package services;
 
 import java.util.List;
+import java.util.Random;
 import java.util.random.*;
 
 import entities.Product;
 
 public class ProductIDGenerator extends IDGenerator<Product> {
     @Override
-    public void setUsageStatus(List<Product> products, Integer generatedId) {
+    protected void setUsageStatus(List<Product> products, Integer generatedId) {
         for (Product product : products) {
             if (product.getId().equals(generatedId)) {
                 isUsing = true;
             }
         }
+    }
+
+    @Override
+    public Integer generateId(List<Product> products) {
+        Random random = new Random();
+        Integer generatedId = random.nextInt(1, Integer.MAX_VALUE);
+        setUsageStatus(products, generatedId);
+        while (isUsing) {
+            generatedId = random.nextInt(1, Integer.MAX_VALUE);
+            setUsageStatus(products, generatedId);
+        }
+        return generatedId;
     }
 }
