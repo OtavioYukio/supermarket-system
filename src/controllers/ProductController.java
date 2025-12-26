d112de83dde2eb4560ea14115541eff21d562226
OtavioYukio <yukiootavio@gmail.com>
2025-12-26 12:37:59 -0300

    feat: add the findById method to the class ProductController

--- src/controllers/ProductController.java
+++ src/controllers/ProductController.java
@@ -7,4 +7,30 @@ import services.ProductService;
 
 public class ProductController {
     private ProductService productService = new ProductService();
+
+    public void save(String name, Double price, String supplier, Integer quantityInStock,
+    Date manufaturingDate, Date expirationDate) {
+        Product product = new Product(name, price, supplier, quantityInStock, manufaturingDate, expirationDate);
+        productService.save(product);
+    }
+
+    public void save(String name, Double price, String supplier, Integer quantityInStock,
+    Date manufaturingDate) {
+        Product product = new Product(name, price, supplier, quantityInStock, manufaturingDate);
+        productService.save(product);
+    }
+
+    public void findById(Integer id) {
+        productService.findById(id);
+    }
+
+    public void findAll() {
+        productService.findAll();
+    }
+
+    public void updateById(Integer id, String name, Double price, String supplier, Integer quantityInStock,
+    Date manufaturingDate, Date expirationDate) {
+        Product product = new Product(name, price, supplier, quantityInStock, manufaturingDate, expirationDate);
+        productService.updateById(quantityInStock, product);
+    }
 }
