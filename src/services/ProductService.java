b02b0e2944fe752ff474e1021f2df4f7fdedd019
OtavioYukio <yukiootavio@gmail.com>
2025-12-26 11:33:20 -0300

    feat: correted the missing generics in the declaration of the productsManagerImp and add the save method to the ProductsService class

--- src/services/ProductService.java
+++ src/services/ProductService.java
@@ -1,8 +1,22 @@
 package services;
 
+import entities.Product;
 import repositories.CrudRepository;
 import repositories.ProductManagerImp;
 
 public class ProductService {
-    CrudRepository productManagerImp = new ProductManagerImp();
+    CrudRepository<Product> productManagerImp = new ProductManagerImp();
+
+    public void save(Product product) {
+        if (product.getPrice() <= 0) {
+            throw new IllegalArgumentException("Invalid product price");
+        }
+        if (product.getQuantityInStock() < 0) {
+            throw new IllegalArgumentException("Invalid quantity of products in stock");
+        }
+        if (product.getName().trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid product name");
+        }
+        productManagerImp.save(product);
+    }
 }
